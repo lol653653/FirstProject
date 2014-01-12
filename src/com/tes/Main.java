31c58d50981cc53412be95f02d4fdecc1095e906
user <user@192.168.10.118>
2014-01-12 20:59:06 +0200

    initialize commit

--- src/com/tes/Main.java
+++ src/com/tes/Main.java
@@ -12,11 +12,92 @@ package com.tes;
 public class Main {
 
     public static void main(String[] args) {
-        Pamuk hammadde = new Pamuk();
+        /*  String str = "Sinan Aleksander Özüak";
+         String[] strDizi = new String[3];
+         strDizi[0] = "Arda Uslu";
+         strDizi[1] = "Murat Özüak";
+         strDizi[2] = "Sinan Aleksander Özüak";
 
-        IplikMakinesi makine = new IplikMakinesi();
+         int i = 100;
+         if (i == 100) {
+         System.out.println("eşit");
+         } else {
+         System.out.println("eşit değil");
 
-        Iplik Urun = makine.pamuguIplikYap(hammadde);
+        sting
+        
+        
+         }
+
+         }
+         */
+        /*for(int i = 0;
+         i< strDizi.length ;
+         i
+
+    
+         ++){
+         if (strDizi[i].equals("Sinan Aleksander Özüak")) {
+         System.out.println(strDizi[i]);
+         }
+
+         }
+
+         }*/
+       int ay = 1;
+        switch (ay) {
+            case 1:
+                System.out.println("Ocak");
+                break;
+            case 2:
+                System.out.println("Şubat");
+                break;
+            case 3:
+                System.out.println("Mart");
+                break;
+            case 4:
+                System.out.println("Nisan");
+                break;
+            case 5:
+                System.out.println("Mayıs");
+                break;
+            case 6:
+                System.out.println("Haziran");
+                break;
+            case 7:
+                System.out.println("Temmuz");
+                break;
+            case 8:
+                System.out.println("Ağustos");
+                break;
+            case 9:
+                System.out.println("Eylül");
+                break;
+            case 10:
+                System.out.println("Ekim");
+                break;
+            case 11:
+                System.out.println("Kasım");
+                break;
+            case 12:
+                System.out.println("Aralık");
+                break;
+            default:
+                System.out.println("Yanlış Ay Değeri");
+        }
     }
 
-}
+        
+        
+        
+        
+        
+        
+        
+        
+             
+        
+        
+        
+        
+      }
