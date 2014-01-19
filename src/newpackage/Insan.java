3365a6c8453ec4998c9ebd333f27b0ff8fcab096
user <user@user-Bilgisayar>
2014-01-19 18:55:17 +0200

    initialize commit

---
+++ src/newpackage/Insan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package newpackage;
+
+/**
+ *
+ * @author user
+ */
+public interface Insan {
+    
+    public void kossun();
+    public String aglasin();
+    public Integer yurusun();
+}
