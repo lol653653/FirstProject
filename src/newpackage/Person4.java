94b41cea1d660122f5f230a996e340e57baa0bdb
user <user@user-Bilgisayar>
2014-01-19 21:04:29 +0200

    initialize commit

--- src/newpackage/Person4.java
+++ src/newpackage/Person4.java
@@ -2,8 +2,8 @@ package newpackage;
 
 public class Person4 {
 
-    public String firstName;
-    public String lastName;
+    private String firstName;
+    private String lastName;
 
     public Person4() {
         }
@@ -17,4 +17,26 @@ public class Person4 {
     public String getFullName() {
         return (firstName + " " + lastName);
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+
+
+
 }
